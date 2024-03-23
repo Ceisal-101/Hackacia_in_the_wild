296f8c96fc0ad69cc9212630b8c9faa40ae7c99d
lxu-wu <xuwuleijie@gmail.com>
2024-03-23 17:27:18 +0100

    TU ES NULLLLLLLLLLLLL

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import './App.css';
 import Chat from './components/chat';
 import WebcamView from './components/webcam';
+import AFrameARComponent from './components/AFrameARComponent';
 
 function App() {
   return (
     <div className="app-container">
-      {/* camera on fullscreen */}
-      <div className="fullscreen-video">
-        <WebcamView />
-      </div>
-      {/* chat */}
+      <AFrameARComponent />
     </div>
   );
 }
