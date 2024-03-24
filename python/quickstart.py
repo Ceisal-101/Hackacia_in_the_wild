58850d451f797031216f5d690bef6bccff1d05b3
Ceisal-101 <139156057+Ceisal-101@users.noreply.github.com>
2024-03-24 02:12:50 +0100

    Update quickstart.py

--- python/quickstart.py
+++ python/quickstart.py
@@ -1,6 +1,6 @@
 from openai import AzureOpenAI
 from dotenv import load_dotenv
-from python.local_to_encode64_image import local_image_to_data_url
+from local_to_encode64_image import local_image_to_data_url
 
 import json
 import os
@@ -12,35 +12,49 @@ api_key= os.getenv("AZURE_OPENAI_API_KEY")
 deployment_name = 'gpt4-003'
 api_version = "2024-02-01" # this might change in the future
 
-image_url = local_image_to_data_url("path_to_your_image")
+#image_url1 = local_image_to_data_url("/Users/ernestomendozagomez/Documents/Screenshots/Photo1.png")
 
 client = AzureOpenAI(
-	api_key=api_key,  
-	api_version=api_version,
-	base_url=f"{api_base}/openai/deployments/{deployment_name}"
+    api_key=api_key,  
+    api_version=api_version,
+    base_url=f"{api_base}/openai/deployments/{deployment_name}"
 )
 
-response = client.chat.completions.create(
-	model=deployment_name,
-	messages=[
-		{ "role": "system", "content": "You are a helpful assistant." },
-		{ "role": "user", "content": [  
-			{ 
-				"type": "text", 
-				"text": "Describe this picture:" 
-			},
-			{ 
-				"type": "image_url",
-				"image_url": {
-					"url": image_url
+def get_prompt(path_to_image):
+	image = local_image_to_data_url(path_to_image)
+	response = client.chat.completions.create(
+		model=deployment_name,
+		messages=[
+			{ "role": "system", "content": "You are an expert in origami folding. You are helping a beginner in the field to \
+			create an origami crane. You need to give concise answers. Be supportive and don't apologize." },
+			{ "role": "user", "content": [  
+				{ 
+					"type": "text", 
+					"text": "I am doing an origami crane. This is my current state. What do I have to do next ?" 
+				},
+				{ 
+					"type": "image_url",
+					"image_url": {
+						"url": image
+					}
 				}
-			}
-		] } 
-	],
-	max_tokens=2000 
-)
+			] }
+		],
+		max_tokens=2000 
+	)
+	return response.choices[0].message.content
 
-with open("response.txt", "w") as f:
-	f.write(response.model_dump_json(indent=2))
+from langchain_openai import AzureOpenAIEmbeddings
 
-print(response.choices[0].message.content)
+embeddings = AzureOpenAIEmbeddings(
+    azure_deployment="embedding-main",
+	model="embedding-ada-002",
+    openai_api_version="2023-05-15",
+)#embedding-ada-002
+
+from langchain_community.vectorstores import FAISS
+db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+
+query = get_prompt("/Users/ernestomendozagomez/Documents/Screenshots/Test1.png")
+docs = db.similarity_search(query)
+print(docs[0].page_content)
